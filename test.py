558ccd5e4925fa2bb26a21cd510650e85f93073b
bgr-ubuntu-vm <bgr.gyk@gmail.com>
2015-02-24 22:06:12 +0100

    DummyModel passes tests

--- test.py
+++ test.py
@@ -2,68 +2,71 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import random
 
-from PyQt5.QtCore import Qt, QAbstractListModel, QVariant, pyqtSlot, QModelIndex
+from PyQt5.QtCore import Qt, QAbstractListModel, QVariant, QModelIndex
 
 from modeltest import ModelTest
 
 
 class DummyModel(QAbstractListModel):
 
-    ROLES = ('hello',)
-    _ROLE_MAP = {}
+    #ROLES = ('hello',)
+    #_ROLE_MAP = {}
 
     def __init__(self, parent=None):
-        self._init_roles()
-        self._items = []
+        self._storage = []
         super(DummyModel, self).__init__(parent)
 
-    def _init_roles(self):
-        keys = list(range(Qt.UserRole + 1, Qt.UserRole + 1 + len(self.ROLES)))
-        self._ROLE_MAP = dict(list(zip(keys, self.ROLES)))
+    #def _init_roles(self):
+        #keys = list(range(Qt.UserRole + 1, Qt.UserRole + 1 + len(self.ROLES)))
+        #self._ROLE_MAP = dict(list(zip(keys, self.ROLES)))
 
-    def roleNames(self):
-        return self._ROLE_MAP
+    #def roleNames(self):
+        #return self._ROLE_MAP
 
-    @pyqtSlot(QModelIndex, result=int)
     def rowCount(self, parent=None):
-        #if parent is None or not parent.isValid():
-            #return 0
-        return len(self._items)
-
-    #@pyqtSlot(QModelIndex, result=bool)
-    #def hasChildren(self, parent=None):
-        #return self.rowCount(parent) > 0
+        if not parent.isValid():
+            return len(self._storage)
+        return 0
 
     def data(self, index, role):
         # sanity checks
         if not index.isValid():
             return QVariant()
 
-        if role not in self._ROLE_MAP:
+        if index.row() >= len(self._storage):
             return QVariant()
 
-        # get item & value
-        #attr = self._ROLE_MAP[role]
-        #item = self._items[index.row()]
-        #value = getattr(item, attr)
-        #return value
-        return self._items[index.row()]
+        if role == Qt.DisplayRole:
+            return self._storage[index.row()]
+        else:
+            return QVariant()
 
-    @pyqtSlot(int)
-    def add_random_items(self, n):
-        self.beginInsertRows(
-            QModelIndex(), self.rowCount(), self.rowCount() + n - 1)
-        self._items.extend([random.randint(1, 100) for _ in range(n)])
+    def insertRows(self, position, rows, parent_index):
+        self.beginInsertRows(QModelIndex(), position, position + rows - 1)
+        for _ in range(rows):
+            self._storage.insert(position, "")
         self.endInsertRows()
+        return True
+
+    def removeRows(self, position, rows, parent_index):
+        self.beginRemoveRows(QModelIndex(), position, position + rows - 1)
+        for _ in range(rows):
+            del self._storage[position]
+        self.endRemoveRows()
+        return True
 
 
 def run():
     model = DummyModel()
     modeltest = ModelTest(model)  # will trigger recheck on every model change
 
-    model.add_random_items(5)
+    for i in range(5):
+        model.insertRows(0, i, None)
+
+    for i in range(5):
+        model.removeRows(0, i, None)
+
 
 
 if __name__ == '__main__':
